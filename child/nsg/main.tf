b9838d4eeed2e7ba946c404284b142cbb1eeeeb1
ssb286 <abc@gmail.com>
2026-08-11 22:17:10 +0530

    corection-1

--- child/nsg/main.tf
+++ child/nsg/main.tf
@@ -1,8 +1,7 @@
 variable "nsg" {}
 
-
 resource "azurerm_network_security_group" "nsg" {
-    for_each = var.nsg
+  for_each            = var.nsg
   name                = each.value.name
   location            = each.value.location
   resource_group_name = each.value.resource_group_name
